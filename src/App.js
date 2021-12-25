c46a8f0ac7508367a0d7753d4ac4d0302ed9e6bb
Christian Mannella <cmannella85@gmail.com>
2021-12-25 12:30:17 +0100

    maps cards
    ciclato il componente card

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
-import { Container,} from 'react-bootstrap'
+import { CardGroup, Col, Container,} from 'react-bootstrap'
 import Hero from './components/Hero'
 import Str from './components/str'
+import { Row } from 'react-bootstrap'
+
+import ImageOne from './img/box-1.png'
+import ImageTwo from './img/box-2.png'
+import ImageTree from './img/box-3.png'
+
 
 export default class App extends Component {
+  state ={
+    cards: [
+      {id: 0, nome: 'Card-1', immagine: ImageTree, testo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel libero at ipsum mollis ullamcorper vitae in mi.'},
+      {id: 1, nome: 'Card-2', immagine: ImageTwo, testo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel libero at ipsum mollis ullamcorper vitae in mi'},
+      {id: 2, nome: 'Card-3', immagine: ImageOne, testo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel libero at ipsum mollis ullamcorper vitae in mi'}
+    ]
+  }
   render() {
     return (
       <Container> 
-        <section>
-          <Hero />
-        </section>
-        <section>
-          <Str />
-        </section>
+          <Hero />      
+        <Row>
+          <CardGroup>
+            {this.state.cards.map(card => (
+              <Str 
+              key={card.id}
+              nome={card.nome}
+              testo={card.testo}
+              immagine={card.immagine}
+              />
+            ))}
+          </CardGroup>
+        </Row>
         
       </Container>
     )
