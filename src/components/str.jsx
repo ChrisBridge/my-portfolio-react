f9813fbe42494bea186ed32da8d0089765f8c868
Christian Mannella <cmannella85@gmail.com>
2021-12-25 22:22:39 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- src/components/str.jsx
+++ src/components/str.jsx
@@ -5,6 +5,7 @@ import { Card, Col } from 'react-bootstrap'
 export default class str extends Component {
     render() {
         return (
+<<<<<<< Updated upstream
           <>
           <Col>
             <Card>
@@ -16,6 +17,54 @@ export default class str extends Component {
             </Card>
           </Col>
           </>
+=======
+            <div>
+<CardGroup>
+
+<Col>
+  <Card>
+    <Card.Img variant="top" src={ImageOne} />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+      This is a wider card with supporting text below as a natural lead-in to 
+      additional content. This card has even longer content than the first to 
+      show that equal height action.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+</Col>
+
+<Col>
+   <Card>
+    <Card.Img variant="top" src={ImageTwo} />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+      This is a wider card with supporting text below as a natural lead-in to 
+      additional content. This card has even longer content than the first to 
+      show that equal height action.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+</Col>   
+
+<Col>
+  <Card>
+    <Card.Img variant="top" src={ImageTree} />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+</Col>    
+</CardGroup>
+            </div>
+>>>>>>> Stashed changes
         )
     }
 }
